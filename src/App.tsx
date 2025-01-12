257776cd5b85bcd95311b05239b10f77fb6cf027
anand <anandvaibhaw000@gmail.com>
2025-01-12 12:43:52 +0530

    built app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Dashboard from "./pages/Dashboard"
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Dashboard />
+    </>
   )
 }
 
